b0b33c48d0395f99264caef2fb2791a953ebc845
hardik <dhardik1430@gmail.com>
2023-06-04 16:16:42 +0530

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import anime from "animejs";
+const Hero = () => {
+  useEffect(() => {
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector(".title-wrapper .letters");
+    textWrapper.innerHTML = textWrapper.textContent.replace(
+      /\S/g,
+      "<span class='letter'>$&</span>"
+    );
+
+    anime.timeline({ loop: false }).add({
+      targets: ".title-wrapper .letter",
+      translateY: ["1.1em", 0],
+      translateZ: 0,
+      duration: 1000,
+      delay: (el, i) => 50 * i,
+    });
+  }, []);
+  return (
+    <>
+      <div className="hero">
+        <h1 class="title-wrapper">
+          <span class="text-wrapper">
+            <span class="letters">The Tooltip</span>
+          </span>
+        </h1>
+        <p className="description">
+          <b>the-tooltip</b> lets you add tooltips to your website with ease. <br /> It is a
+          very lightweight library with no dependencies and very easy to use.
+        </p>
+        <hr />
+      </div>
+    </>
+  );
+};
+
+export default Hero;
