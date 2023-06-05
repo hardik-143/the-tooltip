93ea542435b1c7cf93a5b5da83ca6151fa8accf9
hardik <dhardik1430@gmail.com>
2023-06-06 00:22:15 +0530

    changes

--- src/components/home/Colored.js
+++ src/components/home/Colored.js
@@ -2,35 +2,55 @@ import React from "react";
 import CodeDemo from "../CodeDemo";
 
 const Colored = () => {
-  const arr = ["red", "lime", "yellow", "orange", "blue", "cyan","magneta","silver","grey","maroon","olive","green","purple","teal"];
-  return <>
-  <section>
-    <a href="#colored" className="section-title"># Colored</a>
-    <p className="section-secondary-title">
-        For Colored tooltip, you can use{" "}   
-        <span className="tag bordered">data-ttooltip-color</span>{" "}
-        attribute. <br />
-        It can have following values: <br />
+  const arr = [
+    "blue",
+    "red",
+    "lime",
+    "indigo",
+    "yellow",
+    "orange",
+    "cyan",
+    "silver",
+    "gray",
+    "maroon",
+    "olive",
+    "green",
+    "purple",
+    "pink",
+    "teal",
+    "black",
+    "white",
+  ];
+  return (
+    <>
+      <section>
+        <a href="#colored" className="section-title">
+          # Colored
+        </a>
+        <p className="section-secondary-title">
+          For Colored tooltip, you can use{" "}
+          <span className="tag bordered">data-ttooltip-color</span> attribute.{" "}
+          <br />
+          It can have following values: <br />
         </p>
         <div className="tags-wrapper mb-2">
-            {arr.map((item, index) => {
-                return (
-                    <span key={index} className="tag tag-small tag-blue">
-                        {item}
-                    </span>
-                );
-            })}
+          {arr.map((item, index) => {
+            return (
+              <span key={index} className="tag tag-small tag-blue">
+                {item}
+              </span>
+            );
+          })}
         </div>
         <div className="section-wrapper">
-            {arr.splice(0,4).map((item, index) => {
-                let htmlContent = `<button data-ttooltip data-ttooltip-text="${item} tooltip" data-ttooltip-color="${item}">${item} tooltip</button>`;
-                return (
-                    <CodeDemo key={index} htmlContent={htmlContent} />
-                );
-            })}
+          {arr.splice(0, 4).map((item, index) => {
+            let htmlContent = `<button data-ttooltip data-ttooltip-text="${item} tooltip" data-ttooltip-color="${item}">${item} tooltip</button>`;
+            return <CodeDemo key={index} htmlContent={htmlContent} />;
+          })}
         </div>
-  </section>
-  </>;
+      </section>
+    </>
+  );
 };
 
 export default Colored;
